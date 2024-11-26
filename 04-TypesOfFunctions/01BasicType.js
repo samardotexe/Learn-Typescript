a9f348a837c494fff40e86e8991620017f08daed
samar-ali-ansari <sanardotexe@gmail.com>
2024-11-26 17:32:04 +0530

    Added Basic type of functions

---
+++ 04-TypesOfFunctions/01BasicType.js
@@ -0,0 +1,12 @@
+"use strict";
+//basic Type
+//The function is defined in basic form as in javascript
+//variable type is number and whole function type is number 
+function addition(a, b) {
+    return a + b;
+}
+//here I assigned variable type to only numbers
+function subtraction(x, y) {
+    return x - y;
+}
+//both defining is vald
---
+++ 04-TypesOfFunctions/01BasicType.ts
@@ -0,0 +1,17 @@
+//basic Type
+//The function is defined in basic form as in javascript
+
+
+//variable type is number and whole function type is number 
+function addition(a: number, b: number): number {//valid
+    return a+b
+}
+
+
+//here I assigned variable type to only numbers
+function subtraction(x: number, y: number){//valid
+    return x-y
+}
+
+
+//both defining is vald
